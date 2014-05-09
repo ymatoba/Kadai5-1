a94c55c2aea6a9be57eec5bdc3ede66d29d060b9
g12928my <g12928my+git@s30402.tsuda.ac.jp>
2014-05-09 12:25:31 +0900

    課題5-1 ドライバー　中村恵美 ナビゲーター　的場友希
    メソッド名を変更しました。
    ９９９までの数字を英数字に変換できるようにしました。(途中)

--- IntToEng/src/IntToEng/IntToEng.java
+++ IntToEng/src/IntToEng/IntToEng.java
@@ -10,6 +10,7 @@ package IntToEng;
 import java.util.Scanner;
 
 public class IntToEng {
+	
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
@@ -21,7 +22,8 @@ public class IntToEng {
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
     	
-    	if(n<=19)return nineteen(n);
+    	if(n<=19)return first(n);
+    	else if(n<=999)return second(n);
     	
     	//桁数のお話
     	int length=0;
@@ -31,8 +33,32 @@ public class IntToEng {
         return length+"";
     }
     
-    static String nineteen(int n){
-    	String a[]={"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","tweble","thierteen","fourteen","fifteen","sixtenn","seventeen","eighteen","nineteen"};
+    static String first(int n){
+    	String a[]={"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixtenn","seventeen","eighteen","nineteen"};
     	return a[n];
+    }
+    
+    static String second(int n){
+    	String a[]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    	String b[]={"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    	if(n<=99){
+    		int x=n/10;
+    		int y=n-10*x;    	
+    		if(y==0) return b[x-2];
+    		else return b[x-2]+" "+a[y];
+    	}
+    	
+    	else if(n<=999){
+    		int x=n/100;
+    		int y=(n-x*100)/10;
+    		int z=n-y*10-x*100;
+    		
+    		if(z==0 && y==0) {return a[x]+" hundred ";}
+    		else if(z==0) {return a[x]+" hundred "+b[y-2];}
+    		if(y==0) {return a[x]+ " hundred "+a[z];}
+    		else {return a[x]+" hundred "+b[y-2]+a[z];}
+     	}
+    	
+    	else return "";
     }
 }
