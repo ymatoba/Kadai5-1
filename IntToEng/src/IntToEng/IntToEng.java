34e611b17997f97b6d386d755a83e45a66cfa61a
g12928my <g12928my+git@s30417.tsuda.ac.jp>
2014-05-14 17:34:14 +0900

    ドライバー g12928my 的場友希 ナビゲーター g12933nm 中村恵美
    999までの出力を出来るようにしました。
    今迄十の位以下が10〜19の時に失敗していたため、修正を加えました！

--- IntToEng/src/IntToEng/IntToEng.java
+++ IntToEng/src/IntToEng/IntToEng.java
@@ -10,55 +10,61 @@ package IntToEng;
 import java.util.Scanner;
 
 public class IntToEng {
-	
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-        
-        System.out.println(translateEng(input));
-        
-    }
-    
-    // 数値を英訳する変換するメソッド
-    static String translateEng(int n) {
-    	
-    	if(n<=19)return first(n);
-    	else if(n<=999)return second(n);
-    	
-    	//桁数のお話
-    	int length=0;
-    	if(n<=9){length=1;
-    	}else if(n<=99){length=2;
-    	}
-        return length+"";
-    }
-    
-    static String first(int n){
-    	String a[]={"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixtenn","seventeen","eighteen","nineteen"};
-    	return a[n];
-    }
-    
-    static String second(int n){
-    	String a[]={"zero","one","two","three","four","five","six","seven","eight","nine"};
-    	String b[]={"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-    	if(n<=99){
-    		int x=n/10;
-    		int y=n-10*x;    	
-    		if(y==0) return b[x-2];
-    		else return b[x-2]+" "+a[y];
-    	}
-    	
-    	else if(n<=999){
-    		int x=n/100;
-    		int y=(n-x*100)/10;
-    		int z=n-y*10-x*100;
-    		
-    		if(z==0 && y==0) {return a[x]+" hundred ";}
-    		else if(z==0) {return a[x]+" hundred "+b[y-2];}
-    		if(y==0) {return a[x]+ " hundred "+a[z];}
-    		else {return a[x]+" hundred "+b[y-2]+a[z];}
-     	}
-    	
-    	else return "";
-    }
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+
+		System.out.println(translateEng(input));
+
+	}
+
+	// 数値を英訳する変換するメソッド
+	static String translateEng(int n) {
+
+		if(n<=19)return first(n);
+		else if(n<=999)return second(n);
+
+		//桁数のお話
+		int length=0;
+		if(n<=9){length=1;
+		}else if(n<=99){length=2;
+		}
+		return length+"";
+	}
+
+	static String first(int n){
+		String a[]={"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixtenn","seventeen","eighteen","nineteen"};
+		return a[n];
+	}
+
+	static String second(int n){
+		String a[]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+		String b[]={"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+		if(n<=99){
+			int x=n/10;
+			int y=n-10*x;    	
+			if(y==0) return b[x-2];
+			else return b[x-2]+" "+a[y];
+		}
+
+		else if(n<=999){
+			int x=n/100;//100の位の数
+			int y=(n-x*100)/10;//10の位の数
+			int z=n-y*10-x*100;//1の位の数
+
+			int w;
+			w=n-x*100;
+			if(w<=19&&w>=10){return a[x]+" handred "+first(w);} 		
+			else{
+				if(z==0 && y==0) {return a[x]+" hundred ";}//200
+				if(z==0 && y!=0) {return a[x]+" hundred "+b[y-2];}
+				if(y==0 && z!=0) {return a[x]+ " hundred "+a[z];}//201
+				else{return a[x]+" hundred "+b[y-2]+" "+a[z];}//222
+			}
+		}
+
+		else return "";
+	}
 }
